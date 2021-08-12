5a1dc6caa19b4aded28cb101b9c23bc3f9e55f2d
manolobkno08 <manologm10@hotmail.com>
2021-08-12 00:27:35 -0500

    add task 2

---
+++ 0x00-math_complex/2-modulus.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+ * modulus - function moduls
+ * @c: complex number
+ * Return: c
+ */
+
+double modulus(complex c)
+{
+	double m;
+
+	m = sqrt(pow(c.re, 2) + pow(c.im, 2));
+
+	return (m);
+}
